0d114ebef86482915917e4431189171609203d6b
TusharGupta2820 <tusharwebdevp@gmail.com>
2026-08-31 15:58:35 +0530

    Specify entrypoint api/index.py for backend Python runtime service in vercel.json

--- backend/api/index.py
+++ backend/api/index.py
@@ -3,10 +3,11 @@ import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# Add current directory to python path
+# Add project root directory to python path
 current_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(current_dir))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = get_wsgi_application()
+application = app
